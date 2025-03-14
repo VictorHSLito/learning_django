c9f4d29cf363c300456b170690979b4a8ad08160
Victor Hugo <victorferrarihugo@hotmail.com>
2025-03-14 08:14:36 -0300

    feat: Adiciona páginas individuais para cada tópico

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -13,3 +13,12 @@ def topics(request):
     context = {'topics': topics}
 
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Mostra sobre um assuto em específico"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('data_added')
+    context = {'topic': topic, 'entries': entries}
+
+    return render(request, 'learning_logs/topic.html', context)
