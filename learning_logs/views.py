e913334a9be4a4c156c9b3284a0957b4a31f49e1
Victor Hugo <victorferrarihugo@hotmail.com>
2025-03-13 22:01:38 -0300

    feat: Cria uma nova rota topics.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 def index(request):
     """Retorna a página inicial de learning_logs"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Mostra os assuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+
+    return render(request, 'learning_logs/topics.html', context)
