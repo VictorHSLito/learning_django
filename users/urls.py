fa84cbfb85646f27a90ec37c88065b9865677036
Victor Hugo <victorferrarihugo@hotmail.com>
2025-03-18 20:37:22 -0300

    feat: Cria a view de register

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
@@ -6,5 +7,6 @@ app_name = 'users'
 urlpatterns = [
     # Página de login
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.views.decorators.http import require_POST
 
 
+@require_POST
 def logout_view(request):
     """Faz o logout do usuário"""
     logout(request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
+
+
+def register(request):
+    """Faz o cadastro de um novo usuário"""
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()  # Salva as credenciais do usuário no banco de dados
+            authenticate_user = authenticate(username=new_user.username,
+                                             password=request.POST['password1'])
+            login(request, authenticate_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
