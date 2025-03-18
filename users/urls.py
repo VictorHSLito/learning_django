b7f26a103bd4049aad655336f4e03b602d162f4e
Victor Hugo <victorferrarihugo@hotmail.com>
2025-03-17 21:16:58 -0300

    feat: Cria um protótipo de novos usuários para a aplicação

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+app_name = 'users'
+
+urlpatterns = [
+    # Página de login
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
+]
