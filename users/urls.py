79c677838bd19be47a33f302c2464b5c5f562d28
Victor Hugo <victorferrarihugo@hotmail.com>
2025-03-18 20:07:20 -0300

    feat: Adiciona logout view

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 app_name = 'users'
 
 urlpatterns = [
     # Página de login
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
+from django.urls import reverse
 
-# Create your views here.
+
+def logout_view(request):
+    """Faz o logout do usuário"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
