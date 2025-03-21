c9d51c3932ec24394e45ab6451ac4cfd6077f668
Victor Hugo <victorferrarihugo@hotmail.com>
2025-03-21 19:29:20 -0300

    styles: Adiciona CSS para a página inicial e a de login

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "bootstrap3",
     "learning_logs",
     "users"
 ]
@@ -126,3 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = "/users/login/"
+
+BOOTSTRAP3 = {
+    "include_jquery": True,
+}
