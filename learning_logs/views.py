a69c7be45dd36aea591d4cb37b3a2b9782419d61
Victor Hugo <victorferrarihugo@hotmail.com>
2025-03-20 22:39:18 -0300

    fix: Resolve erro de associação de novos tópicos aos usuários

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 
 from .models import Topic, Entry
@@ -25,6 +25,8 @@ def topics(request):
 def topic(request, topic_id):
     """Função para criar a view sobre um assuto em específico"""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('data_added')
     context = {'topic': topic, 'entries': entries}
 
@@ -39,7 +41,9 @@ def new_topic(request):
     else:
         form = TopicForm(request.POST)
         if form.is_valid():  # Verifica se todas as informações contidas no formulário são válidas
-            form.save()  # Salva os dados do formulário no banco de dados
+            new_topic = form.save(commit=False)  # Salva previamente as informações contidas no formulário
+            new_topic.owner = request.user  # Associa esse novo tópico ao usuário do request
+            new_topic.save()  # Por fim, salva essas informações no banco
             return HttpResponseRedirect(
                 reverse('learning_logs:topics'))  # Redireciona o usuário para a página de topics
 
@@ -72,6 +76,9 @@ def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     print(entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
